def795f5ba099272c7784a2a73dc3a4bb994c7df
agamoni1234 <agamoni@thirdeyedata.io>
2018-09-12 11:01:35 +0530

    deploymnt changes

--- Syra.Deployment/Program.cs
+++ Syra.Deployment/Program.cs
@@ -1,21 +1,61 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Newtonsoft.Json;
+using System.Configuration;
 using System.Threading.Tasks;
+using System;
+using System.Net;
+using System.IO;
+using System.Text;
 
-namespace Syra.Deployment
+namespace Syra.Development
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            string tenantname = ConfigurationSettings.AppSettings["tenant"].ToString();
+            string clientid = ConfigurationSettings.AppSettings["clientId"].ToString();
+            string clientsecret = ConfigurationSettings.AppSettings["clientSecret"].ToString();
+            GetAccessToken(tenantname, clientid, clientsecret);
+            //Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+        public static void GetAccessToken(string tenantName, string clientId, string clientSecret)
+        {
+            var authString = ConfigurationSettings.AppSettings["authString"].ToString() + tenantName + "/oauth2/token";
+            var resourceUrl = ConfigurationSettings.AppSettings["resourceUrl"].ToString();
+            var authenticationContext = new AuthenticationContext(authString);
+            var clientCred = new ClientCredential(clientId, clientSecret);
+            var authenticationResult = authenticationContext.AcquireTokenAsync(resourceUrl, clientCred).Result;
+            var token = authenticationResult.AccessToken;
+            string chatbotname = "";
+            Console.WriteLine("Enter your Chatbot Name : \n");
+            chatbotname = Console.ReadLine();
+            var resourceUri = ConfigurationSettings.AppSettings["staticUri"].ToString() + chatbotname + ConfigurationSettings.AppSettings["apiversion"].ToString();
+            HttpWebRequest request = WebRequest.Create(resourceUri) as System.Net.HttpWebRequest;
+            string location = ConfigurationSettings.AppSettings["resourcelocation"].ToString();
+            var message = JsonConvert.SerializeObject(new { Location = location });
+            Encoding encoding = new UTF8Encoding();
+            byte[] data = encoding.GetBytes(message);
+            request.KeepAlive = true;
+            request.Method = "PUT";
+            request.ContentLength = data.Length;
+            request.ContentType = "application/json";
+            string token_auth = "Bearer " + token;
+            request.Headers.Add("Authorization", token_auth);
+            Stream writer = request.GetRequestStream();
+            writer.Write(data, 0, data.Length);
+            writer.Close();
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            String jsonresponse = "";
+            String temp = null;
+            while ((temp = reader.ReadLine()) != null)
+            {
+                jsonresponse += temp;
+                Console.WriteLine(jsonresponse);
+            }
         }
     }
 }

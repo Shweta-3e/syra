17c93fd6b310fb836f95feeb2e125834741e72ec
santosh <santosh@team.com>
2018-09-25 10:59:21 +0530

    Updated

--- Syra/Controllers/HomeController.cs
+++ Syra/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace Syra.Admin.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
